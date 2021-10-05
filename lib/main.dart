9cd813cdba1755b2ca831202d46e93046cb5d6e6
Victor Hugo <61998936+victorh5@users.noreply.github.com>
2021-10-05 14:42:17 -0400

    adicionada novas frases para o projeto final.

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   var _frases = [
-    "Frase1",
-    "Frase2",
-    "Frase3",
-    "Frase4",
+    "A persistência é o caminho do êxito.",
+    "As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.",
+    "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+    "Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.",
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.",
+    "Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação.",
+    "A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.",
+    "Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo.",
+    "É parte da cura o desejo de ser curado.",
+    "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado."
   ];
 
   var _fraseGerada = "Clique Abaixo para gerar uma frase!";
